fa1b9b97be441ed727008423e8fae281b7430814
Manel-Bousabbat <manelbousabbat@gmail.com>
2021-03-05 22:27:08 +0100

    Styling Elements Dynamically with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,8 +6,15 @@ import { Component } from '@angular/core';
 export class ServerComponent{
 serverId = 10;
 serverStatus = 'offline';
+constructor() {
+  this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline' ;
+}
   // tslint:disable-next-line:typedef
  getServerStatus() {
   return this.serverStatus;
+}
+// tslint:disable-next-line:typedef
+getColor(){
+  return this.getServerStatus() === 'online' ? 'green' : 'red' ;
 }
 }
